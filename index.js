61473eacd343979e4a759ddcdf0c9d7aa640a065
Riveralo <rivera.ldg@gmail.com>
2026-03-02 17:23:27 -0600

    Actualizando URL producción

--- index.js
+++ index.js
@@ -162,7 +162,9 @@ app.post('/generar-pdf', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
